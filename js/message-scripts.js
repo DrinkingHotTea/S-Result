76a2eeb11521d4e78707ec9197eebcf72b498320
DrinkingHotTea <95166975+DrinkingHotTea@users.noreply.github.com>
2022-03-17 00:32:29 +0300

    Add files via upload

--- js/message-scripts.js
+++ js/message-scripts.js
@@ -18,6 +18,10 @@ if (document.body.classList.contains('_mobile')) {
     peopleItemAll.forEach(e => {
         e.classList.add('mobile-visible')
     })
+} 
+else {
+    const messageField = document.querySelector('.message__field')
+    messageField.style.paddingRight = '10px'
 }
 
 // Первые 6 констант тоже копировать
@@ -42,9 +46,21 @@ const sidebarArrow = document.querySelector('.sidebar__arrow')
 const sidebarInfoClose = document.querySelector('.sidebar-info__close')
 const peopleContent = document.querySelector('.people__content')
 const sidebarItems = document.querySelector('.sidebar__items')
+const messageContent = document.querySelector('.message__content')
 
 const sideItemForCopy = sidebarItems.innerHTML
 const sidebarItem = document.querySelector('.sidebar__item').classList.add('hidden')
+
+const messageContentForCopy = messageContent.innerHTML
+const messageItem = document.querySelector('.message__item').classList.add('hidden')
+
+const myMessage = document.querySelector('.message__my-for-copy')
+const friendMessage = document.querySelector('.message__friend-for-copy')
+const myMessageForCopy = myMessage.innerHTML
+const friendMessageForCopy = friendMessage.innerHTML
+
+let tempBoolean = true
+let secBoolean = true
 
 const namesArray = [
     'Александр', 'Алексей', 'Анатолий', 'Андрей', 'Антон', 'Артём', 'Артур', 'Борис', 'Вадим', 'Василий', 'Виктор', 'Виталий', 'Владимир', 'Владислав', 'Вячеслав', 
@@ -54,10 +70,18 @@ const namesArray = [
     'Надежда', 'Наталья', 'Оксана', 'Ольга', 'Полина', 'Светлана', 'Снежана', 'Тамара', 'Юлия', 'Яна'
 ]
 
+const answerArray = [
+    'Привет', 'Ку', 'Как дела', 'Все хорошо', 'Че каво', 'Нормально', 'Да', 'Нет', 'Как интересно', 'Интересно', 'Не интересно', 'Здравствуй', 'Что ты хочешь?', 
+    'Что тебе нужно?', 'Кто тебе нужен?', 'Давай', 'Пошли в дискорд', 'Хочешь мой номер?', 'Дать номер?', 'Дай свой номер', 'Что мне сделать?', 'Хорошо', 'Я не хочу общаться', 
+    'Я не в настроении', 'Потому что', 'Не хочу', 'Хочу', 'Сижу', 'Пишу', 'Читаю', 'Просто', 'Давай созвонимся', 'Я не алиса, но что-нибудь отвечу', 'Что тебе ещё написать?', 
+    'Было бы хорошо', 'Да неее', 'Искуственный интеллект в действии'
+]
+
 createFriendsBlocks() // Создание блоков с друзьями
 nameFunc() // Функция с именами
 addFriendsFunc() // Кнопки добавления в друзья
 friendsClickFunc() // Функционал сайдбара
+messageBlockHeight() // Высота блока с переписками
 
 // Обязательно для копирования
 header.classList.add('other-header')
@@ -86,6 +110,8 @@ sidebarInfoClose.addEventListener('click', () => {
         sidebarInfo.remove()
     }
 })
+
+window.addEventListener('resize', messageBlockHeight)
 
 // Это тоже
 function menuButtonFunc() {
@@ -123,6 +149,7 @@ function openMessanger() {
 
         peopleSubLine.classList.remove('visible')
         messageSubLine.classList.add('visible')
+        messageBlockHeight()
     }
 }
 
@@ -136,13 +163,36 @@ function addFriendsFunc() {
         peopleButton[i].addEventListener('click', () => {
             if (!currentItem.classList.contains('added')) {
                 peopleButton[i].innerHTML = '<span class="material-icons">done</span>'
+
                 sidebarItems.insertAdjacentHTML('beforeend', sideItemForCopy)
+                messageContent.insertAdjacentHTML('beforeend', messageContentForCopy)
+
+                const messageItem = document.querySelectorAll('.message__item')
+                messageItem[messageItem.length - 1].querySelector('.message__my-message').classList.add('hidden')
+                messageItem[messageItem.length - 1].querySelector('.message__friend-message').classList.add('hidden')
 
                 const name = peopleButton[i].previousElementSibling.innerHTML
                 const sidebarLastItem = document.querySelectorAll('.sidebar__item')
                 const nameForChenge = sidebarLastItem[sidebarLastItem.length - 1].querySelector('.item__name')
+
+                const messageLastItem = document.querySelectorAll('.message__item')
+                const messageNameForChenge = messageLastItem[sidebarLastItem.length - 1].querySelector('.friend-name__text')
+
                 nameForChenge.innerHTML = name
+                messageNameForChenge.innerHTML = name
                 friendsClickFunc()
+
+                if (secBoolean) {
+                    const messageItem = document.querySelectorAll('.message__item')
+                    messageItem.forEach(e => {
+                        e.classList.add('hidden')
+                    })
+                    messageItem[1].classList.remove('hidden')
+                    secBoolean = false
+                }
+
+                const messageInfo = document.querySelector('.message__info')
+                messageInfo.style.display = 'none'
             }
             currentItem.classList.add('added')
         })
@@ -189,9 +239,21 @@ function sidebarFunc() {
 
 function friendsClickFunc() {
     const sidebarItem = document.querySelectorAll('.sidebar__item')
-    for (var i = 0; i < sidebarItem.length; i++) {
+    for (let i = 0; i < sidebarItem.length; i++) {
         sidebarItem[i].onclick = () => {
             openMessanger()
+            messageLink.scrollIntoView({
+                block: "end"
+            })
+
+            const messageItem = document.querySelectorAll('.message__item')
+            messageItem.forEach(e => {
+                e.classList.add('hidden')
+            })
+            messageItem[i].classList.remove('hidden')
+            messageBlockHeight()
+            sendMessage()
+            
             if (sidebar.classList.contains('visible-sidebar')) {
                 sidebar.classList.remove('visible-sidebar')
             }
@@ -209,4 +271,65 @@ function createFriendsBlocks() {
     for (let i = 0; i < randomNumber; i++) {
         peopleContent.insertAdjacentHTML('beforeend', friendForCopy)
     }
+}
+
+function messageBlockHeight() {
+    const windowHeight = window.innerHeight
+    const messageItem = document.querySelectorAll('.message__item')
+    for (let i = 0; i < messageItem.length; i++) {
+        const messageItemPosition = messageItem[i].getBoundingClientRect().top
+        messageItem[i].style.height = `${windowHeight - messageItemPosition - 10}px`
+    }
+}
+
+function sendMessage() {
+    const textareaAll = document.querySelectorAll('.textarea')
+    const instrumentsButton = document.querySelectorAll('.instruments__button')
+    const messageFieldAll = document.querySelectorAll('.message__field')
+    const instrumentsAlert = document.querySelectorAll('.instruments__alert')
+    for (let i = 0; i < textareaAll.length; i++) {
+        instrumentsButton[i].onclick = () => {
+            let thisArea = textareaAll[i]
+            let thisField = messageFieldAll[i]
+            let thisAlert = instrumentsAlert[i]
+
+            if (thisArea.value.length <= 0) {
+                if (tempBoolean) {
+                    thisAlert.classList.add('visible')
+                    tempBoolean = false
+                    setTimeout(() => {
+                        thisAlert.classList.remove('visible')
+                        tempBoolean = true
+                    }, 1000);
+                }
+            } 
+            else {
+                thisField.insertAdjacentHTML('beforeend', myMessageForCopy)
+                const lastMessage = thisField.querySelectorAll('.my-message__text')
+                lastMessage[lastMessage.length - 1].textContent = thisArea.value
+                thisArea.value = ''
+
+                let scrollToLast = thisField.scrollHeight
+                thisField.scrollTo({
+                    top: scrollToLast
+                })
+
+                setTimeout(() => {
+                    messageAnswer(thisField)
+                }, 2000);
+            }
+        }
+    }
+}
+
+function messageAnswer(thisField) {
+    let randomFriendNumber = Math.floor(Math.random() * (answerArray.length - 1))
+    thisField.insertAdjacentHTML('beforeend', friendMessageForCopy)
+    const lastFriendMessage = thisField.querySelectorAll('.friend-message__text')
+    lastFriendMessage[lastFriendMessage.length - 1].textContent = answerArray[randomFriendNumber]
+
+    let scrollToLast = thisField.scrollHeight
+    thisField.scrollTo({
+        top: scrollToLast
+    })
 }
